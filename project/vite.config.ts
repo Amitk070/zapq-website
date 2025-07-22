46ff1a60e121b55ba6834c862a5e95884b132537
Amitk070 <kumarami663@gmail.com>
2025-07-22 19:03:01 +0530

    Update vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './', // ✅ forces relative paths in index.html
+  base: '/code/', // ✅ required for Vercel subpath deployment
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react']
